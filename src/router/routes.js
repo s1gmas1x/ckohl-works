c16a05046b17e3144a418e0c898079ba60e3a693
Chad Kohl <ckohl401@gmail.com>
2026-07-11 08:47:28 -0600

    feat: add Margot's Pizza demo

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,6 +4,7 @@ const routes = [
     component: () => import('@/layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('@/pages/IndexPage.vue') },
+      { path: 'margots-pizza', component: () => import('@/pages/MargotsPizzaPage.vue') },
       { path: 'second', component: () => import('@/pages/SecondPage.vue') },
     ],
   },
